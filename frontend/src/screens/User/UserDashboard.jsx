1e96bc705a04016fc93ccb84638451d081deb593
itxSaaad <saadstudent.cs@gmail.com>
2023-05-27 17:33:51 +0500

    Added: User Dashboard

---
+++ frontend/src/screens/User/UserDashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Dashboard from "../../components/Dashboard/Dashboard";
+
+const UserDashboard = () => {
+  return <Dashboard variant="user" />;
+};
+
+export default UserDashboard;
