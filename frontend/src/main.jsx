b16d86f55048e8a21a566bf624e0fe869df05d8a
itxSaaad <saadstudent.cs@gmail.com>
2023-05-30 20:45:11 +0500

    Added: User Login, Register & Logout Constants

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import axios from "axios";
 
 import App from "./App";
 import "./index.css";
 import store from "./redux/store";
+
+axios.defaults.baseURL = "http://localhost:3000";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
