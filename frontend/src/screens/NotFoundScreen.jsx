84b9c04be351ab382f9a21b9a35f1bd42575a373
itxSaaad <saadstudent.cs@gmail.com>
2023-06-04 17:19:36 +0500

    Added: Not Found Screen

---
+++ frontend/src/screens/NotFoundScreen.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../components/Button";
+import Loader from "../components/Loader";
+
+const NotFoundScreen = () => {
+  return (
+    <section className="min-h-screen flex flex-row justify-center items-center bg-gray-800 text-white py-20">
+      <div className="mx-auto max-w-screen-xl w-full flex flex-col justify-center items-center px-4 sm:px-8 lg:px-6">
+        <div className="flex flex-col justify-center items-center">
+          <i className="fas fa-link-slash fa-spin fa-fade text-6xl text-gray-500 mb-4"></i>
+          <span className="text-gray-500 text-2xl">Broken Link</span>
+        </div>
+
+        <h1 className="text-4xl text-center font-bold mt-4 mb-2">
+          404 - Page Not Found
+        </h1>
+        <p className="text-lg text-center mb-4 text-gray-500">
+          The requested page could not be found.
+        </p>
+        <Link to="/">
+          <Button variant="secondary" className="rounded-md mr-2">
+            <i className="fas fa-arrow-left mr-2"></i>
+            Go back to the Homepage
+          </Button>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default NotFoundScreen;
--- frontend/src/screens/Technician/TechProfileScreen.jsx
+++ frontend/src/screens/Technician/TechProfileScreen.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 import { getTechnicianDetails } from "../../redux/actions/technicianActions.js";
 
+import Button from "../../components/Button";
 import Loader from "../../components/Loader";
 import Message from "../../components/Message";
 import Profile from "../../components/Profile";
 
 const TechProfileScreen = () => {
   const { id } = useParams();
-
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -19,6 +20,21 @@ const TechProfileScreen = () => {
 
   const technicianDetails = useSelector((state) => state.technicianDetails);
   const { loading, error, techUser } = technicianDetails;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const handleBookTechncian = () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to book this Technician?"
+    );
+
+    if (confirmed) {
+      // Perform the booking action
+      console.log("Book Technician", techUser._id, userInfo.user._id);
+      alert("Technician Booked Successfully");
+    }
+  };
 
   return (
     <section className="min-h-screen flex flex-row justify-center items-center bg-gray-800 text-white py-20">
@@ -31,7 +47,27 @@ const TechProfileScreen = () => {
         ) : error ? (
           <Message>{error}</Message>
         ) : (
-          <Profile user={techUser} userType="technician" />
+          <>
+            <div className="w-3/4 flex flex-row justify-between ">
+              <Link to="/technicians">
+                <Button variant="secondary" className="rounded-md mr-2">
+                  <i className="fas fa-arrow-left mr-2"></i>
+                  Go Back
+                </Button>
+              </Link>
+              {userInfo ? (
+                <Button
+                  variant="primary"
+                  className="rounded-md"
+                  onClick={handleBookTechncian}
+                >
+                  <i className="fas fa-user-check mr-2"></i>
+                  Book Technician
+                </Button>
+              ) : null}
+            </div>
+            <Profile user={techUser} userType="technician" />
+          </>
         )}
       </div>
     </section>
