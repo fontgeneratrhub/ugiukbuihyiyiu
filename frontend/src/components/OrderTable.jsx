b1d63f7e66e510605ec405f73cee6ff64ced22be
itxSaaad <saadstudent.cs@gmail.com>
2023-06-05 11:34:55 +0500

    Added: Pending and Done Orders Tables

--- frontend/src/components/OrderTable.jsx
+++ frontend/src/components/OrderTable.jsx
@@ -8,55 +8,100 @@ const OrderTable = ({
   handleStatus,
   entityType,
 }) => {
-  const isUserLoggedIn = entityType === "user" ? true : false;
+  const isUserLoggedIn = entityType === "user";
+
+  // Filter orders based on status
+  const pendingOrders = data.filter(
+    (order) => order.status === "Pending" || order.status === "pending"
+  );
+  const doneOrders = data.filter((order) => order.status === "Done");
 
   return (
     <div className="overflow-x-auto">
-      <table className="bg-gray-700 w-full table-auto border-collapse border-2 border-gray-400 rounded-lg text-center overflow-hidden whitespace-no-wrap">
-        <thead className="bg-gray-800 h-10 uppercase font-bold">
-          <tr>
-            {columns.map((column) => (
-              <th key={column}>{column.replace(/([A-Z])/g, " $1").trim()}</th>
-            ))}
-            {isUserLoggedIn ? <th>Mark as Completed</th> : null}
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody className="bg-gray-600 text-gray-100">
-          {data.map((row) => (
-            <tr key={row._id}>
+      <div className="mb-6">
+        <h2 className="text-xl font-bold mb-2">Pending Orders</h2>
+        <table className="bg-gray-700 w-full table-auto border-collapse border-2 border-gray-400 rounded-lg text-center overflow-hidden whitespace-no-wrap">
+          <thead className="bg-gray-800 h-10 uppercase font-bold">
+            <tr>
               {columns.map((column) => (
-                <td
-                  key={column}
-                  className="border border-gray-500 px-4 py-2 sm:px-2 sm:py-1"
-                >
-                  {row[column]}
-                </td>
+                <th key={column}>{column.replace(/([A-Z])/g, " $1").trim()}</th>
               ))}
-              {isUserLoggedIn ? (
+              {isUserLoggedIn ? <th>Mark as Completed</th> : null}
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody className="bg-gray-600 text-gray-100">
+            {pendingOrders.map((row) => (
+              <tr key={row._id}>
+                {columns.map((column) => (
+                  <td
+                    key={column}
+                    className="border border-gray-500 px-4 py-2 sm:px-2 sm:py-1"
+                  >
+                    {row[column]}
+                  </td>
+                ))}
+                {isUserLoggedIn ? (
+                  <td className="border border-gray-500">
+                    <Button
+                      variant="primary"
+                      className="rounded-md"
+                      onClick={() => handleStatus(row._id)}
+                    >
+                      <i className="fas fa-circle-check"></i>
+                    </Button>
+                  </td>
+                ) : null}
                 <td className="border border-gray-500">
                   <Button
-                    variant="primary"
+                    variant="danger"
                     className="rounded-md"
-                    onClick={() => handleStatus(row._id)}
+                    onClick={() => handleDelete(row._id)}
                   >
-                    <i className="fas fa-clipboard-list-check"></i>
+                    <i className="fas fa-trash"></i>
                   </Button>
                 </td>
-              ) : null}
-              <td className="border border-gray-500">
-                <Button
-                  variant="danger"
-                  className="rounded-md"
-                  onClick={() => handleDelete(row._id)}
-                >
-                  <i className="fas fa-trash"></i>
-                </Button>
-              </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <h2 className="text-xl font-bold mb-2">Done Orders</h2>
+        <table className="bg-gray-700 w-full table-auto border-collapse border-2 border-gray-400 rounded-lg text-center overflow-hidden whitespace-no-wrap">
+          <thead className="bg-gray-800 h-10 uppercase font-bold">
+            <tr>
+              {columns.map((column) => (
+                <th key={column}>{column.replace(/([A-Z])/g, " $1").trim()}</th>
+              ))}
+              <th>Delete</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="bg-gray-600 text-gray-100">
+            {doneOrders.map((row) => (
+              <tr key={row._id}>
+                {columns.map((column) => (
+                  <td
+                    key={column}
+                    className="border border-gray-500 px-4 py-2 sm:px-2 sm:py-1"
+                  >
+                    {row[column]}
+                  </td>
+                ))}
+                <td className="border border-gray-500">
+                  <Button
+                    variant="danger"
+                    className="rounded-md"
+                    onClick={() => handleDelete(row._id)}
+                  >
+                    <i className="fas fa-trash"></i>
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
