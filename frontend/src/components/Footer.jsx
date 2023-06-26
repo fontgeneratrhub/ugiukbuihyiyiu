9168ac6c68d29b2987f3bc3948eb1d6535cedffa
itxSaaad <saadstudent.cs@gmail.com>
2023-06-26 12:12:09 +0500

    Added: Technician and Admin Login Button to Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Button from "./Button";
+
 const Footer = () => {
   return (
     <footer className="bg-gray-900 w-full shadow-md transition-all duration-500 ">
@@ -40,10 +42,28 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center py-4 border-t border-gray-400">
+      <div className="flex sm:flex-row flex-col items-center justify-evenly py-4 border-t border-gray-400">
+        <Link to="/technician/login">
+          <Button
+            variant="outline"
+            className="p-2 rounded-md text-white text-sm"
+          >
+            Login as Technician
+          </Button>
+        </Link>
+
         <p className="text-gray-400 text-sm">
           &copy; 2023 Kariger.com. All rights reserved.
         </p>
+
+        <Link to="/admin/login">
+          <Button
+            variant="outline"
+            className="p-2 rounded-md text-white text-sm"
+          >
+            Login as Admin
+          </Button>
+        </Link>
       </div>
     </footer>
   );
